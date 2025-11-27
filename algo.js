de2910e003a2000ce1c9bb3be31188661d5f4bfb
AsokTamang <ashoktmg2058@gmail.com>
2025-11-26 20:30:58 -0500

    map method

---
+++ algo.js
@@ -0,0 +1,33 @@
+function getStockData() {
+    return {
+        name: 'QtechAI',
+        sym: 'QTA',
+        price: Math.random(0,3).toFixed(2),  //here we are getting the random numbers between 0 and 3 and then fixing the decimal upto 2
+        time:new Date().toLocaleTimeString()
+    }
+}
+console.log(getStockData())  
+
+
+const playlistHtml = []
+
+
+    playlistHtml.push(
+        
+`<section class="card">
+    <div class="card-start">
+        <img src="/images/${playlistArr[i].albumArt}">
+    </div>
+        <div class="card-mid">
+            <h4 class="card-title">${playlistArr[i].title}</h4>
+            <p class="card-artist">${playlistArr[i].artist}</p>
+        </div>
+    <div class="card-end">
+        <p class="card-menu">...</p>
+    </div>
+</section>
+    `
+    )
+
+
+document.getElementById('container').innerHTML = playlistHtml
