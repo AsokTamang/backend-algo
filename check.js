958d804d4d2bfcb417b1092cc90418a4edbd39dd
AsokTamang <ashoktmg2058@gmail.com>
2025-11-28 11:14:20 -0500

    spread property

--- check.js
+++ check.js
@@ -34,10 +34,35 @@ Challenge:
    'accessPremiumFeature' both exists AND is true.
    u
 */ 
-return Object.hasOwn(userObj,prop) && userObj.hasOwnProperty(prop)  //hasown is based on the Object whereas hasownproperty is based on the object data itself
+return Object.hasOwn(userObj,prop) && userObj.hasOwnProperty(prop) && userObj[prop] == true  //hasown is based on the Object whereas hasownproperty is based on the object data itself
 
 }
 
 console.log(canAccessPremiumFeature(user1, 'accessPremiumFeature'))
 console.log(canAccessPremiumFeature(user2, 'accessPremiumFeature')) 
 
+const averageSharePriceByMonthQ1 = [109.6, 103.3, 89.4]
+const averageSharePriceByMonthQ2 = [109.3, 126.1, 103.3]
+const averageSharePriceByMonthQ3 = [120.8, 102.3, 106.8]
+const averageSharePriceByMonthQ4 = [110.9, 119.8, 113.7]
+
+function findPriceExtremes(arr){
+/*
+Challenge:
+2. Find the highest number from the array 
+   and store it in the const 'highest'. 
+3. Find the lowest number from the array 
+   and store it in the const 'lowest'. 
+*/
+    const highest = Math.max(...arr)
+    const lowest = Math.min(...arr)
+    console.log(`The highest average share price was ${highest}`)
+    console.log(`The lowest average share price was ${lowest}`)
+}
+
+/*
+Challenge:
+1. Call this function with one array holding 
+   all of the data from the 4 arrays above.
+*/
+findPriceExtremes([...averageSharePriceByMonthQ1,...averageSharePriceByMonthQ2,...averageSharePriceByMonthQ3,...averageSharePriceByMonthQ4])
