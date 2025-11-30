ad8713fc203fecbc11752358c510592ea497a5dc
AsokTamang <ashoktmg2058@gmail.com>
2025-11-29 22:22:10 -0500

    binding method

--- check.js
+++ check.js
@@ -89,3 +89,22 @@ studentDetailsCopy.address.name = 'Pokhara'
 
 console.log(studentDetailsCopy)
 console.log(studentDetails)
+
+
+const button = document.getElementById('btn')
+
+const product = {
+    name: 'Vanilla Lip Gloss',
+    sku: 'w234fg',
+    stock: 276,
+    getProductInfo: function() {
+        console.log(`Stock level for ${this.name} (SKU: ${this.sku}): ${this.stock}`)
+    }
+}
+
+
+button.addEventListener('click', product.getProductInfo.bind(product))
+
+
+
+
