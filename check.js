776f254c4bab909c72aab96a4c36a216ce730baa
AsokTamang <ashoktmg2058@gmail.com>
2025-12-01 11:21:03 -0500

    constructor function

--- check.js
+++ check.js
@@ -107,4 +107,49 @@ button.addEventListener('click', product.getProductInfo.bind(product))
 
 
 
+/*
+Challenge:
+1. Create a constructor function called 'Character'.
+2. Give it 'name' and 'collectedItemsArr' properties. 
+    - 'name' should hold the character’s name.
+	- 'collectedItemsArr' should hold an array of items. 
+       Initialise it to an empty array.
+3. Create an 'addItem' method which takes in an item as 
+   a parameter and adds it to 'collectedItemsArr'.
+4. The addItem method should log out a sentence like 
+   `Merlin now has: wand, map, potion`.
+5. Check it’s working by creating several instances of 
+   Character and adding items to their arrays.
+*/
+
+/*
+Challenge:
+1. Create a constructor function called 'Character'.
+2. Give it 'name' and 'collectedItemsArr' properties. 
+    - 'name' should hold the character’s name.
+	- 'collectedItemsArr' should hold an array of items. 
+       Initialise it to an empty array.
+3. Create an 'addItem' method which takes in an item as 
+   a parameter and adds it to 'collectedItemsArr'.
+4. The addItem method should log out a sentence like 
+   `Merlin now has: wand, map, potion`.
+5. Check it’s working by creating several instances of 
+   Character and adding items to their arrays.
+*/
+
+
+class Character {
+  constructor(name, collectedItemsArr = []) {
+    this.name = name
+    this.collectedItemsArr = collectedItemsArr
+    this.addItem = function (item) {   //this function takes an item as a parameter which is used for adding to the collectedItemsArr
+      this.collectedItemsArr.push(item)
+      console.log(`${this.name} now has : ${this.collectedItemsArr.join(',')}`)
+    }
+
+
+  }
+}
+
+
 
