2223f06c874e44024d6b4e3bd198caecd14e1e9f
AsokTamang <ashoktmg2058@gmail.com>
2025-12-01 12:51:05 -0500

    function inheritance using chaining method

--- check.js
+++ check.js
@@ -154,4 +154,45 @@ class Character {
 
 
 
+  function Event(name, location, date) {
+    this.name = name
+    this.location = location
+    this.date = date
+}
+
+Event.prototype.getDetails = function() {
+    return `Event: ${this.name}, Location: ${this.location}, Date: ${this.date}`
+}
+w
+function Conference(name,location,date,keynoteSpeaker){
+    Event.call(this,name,location,date)   //here we are inheriting the properties of event to the conference
+    this.keynoteSpeaker=keynoteSpeaker
+}
+Conference.prototype=Object.create(Event)   //here we are inheriting the method of Event
+Conference.prototype.constructor = Conference
+Conference.prototype.getDetails=function(){   //here we are creating a method called getdetails of the function Conference
+  const basedetails = Event.prototype.getDetails.call(this)    //then we are calling the event's version of getdetails here
+  return `${basedetails} keynoteSpeaker:${this.keynoteSpeaker}`
+}  
+
+
+
+/*`
+Challenge:
+    1. Set up a constructor for 'Conference' which 
+       should take in 'keynoteSpeaker' as a parameter. 
+    2. 'Conference' should inherit from 'Event'.
+    3. 'Conference' should have its own method 'getDetails'
+       which calls Event's getDetails method and returns a string 
+       with name, location, date, and keynote speaker.  
+    4. When you set up an instance of Conference and call 
+       getDetails you should log out: 
+       Event: 10 Nights of JS, Location: Scrimba HQ, Date: 2025-09-29 Keynote Speaker: Ashley Smith
+       📝 The new method should be on the prototype. 
+*/
+
+const conference = new Conference("10 Nights of JS", "Scrimba HQ", "2025-09-29", "Ashley Smith")
+console.log(conference.getDetails())
+
+
 
