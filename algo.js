84fbaa8de5437c28397f049a9361e4771cd727c9
AsokTamang <ashoktmg2058@gmail.com>
2025-12-01 23:51:39 -0500

    map object challenge

--- algo.js
+++ algo.js
@@ -182,7 +182,7 @@ const keys = ()=>bookKeys.forEach((book)=>console.log(book))  //this prints the 
 const titles = ()=>bookKeys.forEach((book)=>console.log(books[book]))  //this prints the title based on the keys
 
 const bookvalues = Object.values(books)
-bookvalues.forEach((bv)=bv.price)
+bookvalues.forEach((bv)=console.log(bv.price))
 /*
 Output: b001
         b002
@@ -193,3 +193,36 @@ Output: To Kill a Mockingbird
         The Great Gatsby
         Moby Dick
 */
+
+
+const athlete1 = { name: 'Alice', averageTime10KmMins: 58.3 }
+const athlete2 = { name: 'Dave', averageTime10KmMins: 53.2 }
+const athlete3 = { name: 'Micky', averageTime10KmMins: 64.5 }
+const athlete4 = { name: 'Judy', averageTime10KmMins: 66.0 }
+
+/* Challenge */
+
+/* 1. Create a map object "athletes" to store the athletes. */
+const athletes = new Map()
+
+function addAthlete(athlete, time) {
+/* 2. This function should add athletes to the "athletes" map. */
+  athletes.set(athlete,time)   //here we are setting the time to this certain athlete inside the map athletes
+}
+
+function getSummary(){
+/* This function should make the following appear in the console */
+    athletes.forEach((value,key)=>console.log(`${key.name}'s average time is ${key.averageTime10KmMins} but today ${key.name} achieved ${value}`))
+    
+//Alice's average time is 58.3 but today Alice achieved 57.3
+//Dave's average time is 53.2 but today Dave achieved 61.1
+//Micky's average time is 64.5 but today Micky achieved 59.9
+//Judy's average time is 66 but today Judy achieved 61.6
+}
+
+addAthlete(athlete1, 57.3)
+addAthlete(athlete2, 61.1)
+addAthlete(athlete3, 59.9)
+addAthlete(athlete4, 61.6)
+
+getSummary()
