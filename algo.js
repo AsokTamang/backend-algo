d982c9868c51663d237658dc7625a553a0c0a30a
AsokTamang <ashoktmg2058@gmail.com>
2025-12-02 00:11:04 -0500

    throttling

--- algo.js
+++ algo.js
@@ -246,3 +246,37 @@ addTag('history')
 addTag('history')
 
 postTags.forEach((tag) => console.log(tag))
+
+
+
+function scorePoint(playerName) {
+    let score = 0
+    return {   //here we are returning the object which has the key-name and the function as a value
+        displayScore: function () {
+            console.log(`${playerName} has ${score} points`)
+        },
+        incrementScore: function(){
+          score++
+        },
+        decrementScore: function(){
+          score--
+        }
+/*
+Challenge:
+   1. Add two more functions to this object. One to increment 
+      the score and one to decrement the score. Call those functions 
+      a couple of times for each player and then call displayScore 
+      to check it’s working. 
+*/
+    }
+}
+
+const player1 = scorePoint('Vicky')
+const player2 = scorePoint('Leo')
+player1.incrementScore()
+player2.incrementScore()
+player1.decrementScore()
+player2.decrementScore()
+player1.displayScore()
+player2.displayScore()
+
