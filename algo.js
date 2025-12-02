c81f079065595ef2caf2d68a1016b2e08db45f17
AsokTamang <ashoktmg2058@gmail.com>
2025-12-02 09:16:26 -0500

    closure function

--- algo.js
+++ algo.js
@@ -280,3 +280,54 @@ player2.decrementScore()
 player1.displayScore()
 player2.displayScore()
 
+
+
+
+/* 
+Challenge: 
+    1. Write a function that simulates a simple bank account. 
+       The function should store the balance and enable users  
+       to deposit money, withdraw money, and check the balance.
+
+Requirements:
+
+- The account balance should start at zero.
+
+- The function should return an object containing 
+  3 functions. One for depositing money, one for 
+  withdrawing money, and a getBalance function which 
+  logs the balance and the account holder's name.
+
+- Deposits should add to the balance, and withdrawals should subtract 
+  from it. 
+*/
+
+function createBankAccount(name) {
+  let balance = 0 //initially the balance is 0
+    
+// Write logic here 
+return (
+  {
+    withdraw: function(amount){
+      balance-=amount
+      return balance
+
+    },
+    deposit: function(amount) {
+      balance+=amount
+      return balance
+    },
+    getBalance:function() {
+       console.log(`balance of ${name} is ${balance}.`)
+    }
+  }
+)   
+}
+
+const daveAccount = createBankAccount('dave')
+const wendyAccount = createBankAccount('wendy')
+console.log(daveAccount.deposit(50))
+console.log(daveAccount.withdraw(20))
+
+
+// Test your code by calling the functions.
