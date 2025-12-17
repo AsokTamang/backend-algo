6f52edf0be12974ec71bb2657e169e67a57ca4d1
AsokTamang <ashoktmg2058@gmail.com>
2025-12-17 14:07:13 -0500

    checking bugs

--- practype.js
+++ practype.js
@@ -25,10 +25,36 @@ function addNewPizza(pizzaobj) {
  *    (e.g. { pizza: selectedPizzaObjectFromStep1, status: "ordered" })
  * 4. returns the new order object (just in case we need it later)
  */
+let nextOrderId = 1;
 function placeOrder(pname) {
-  a = menu.find((pobj) => (pobj.name === pname)); //here we are using find inorder to return the single object
+  a = menu.find((pobj) => pobj.name === pname); //here we are using find inorder to return the single object
   cashInRegister += a.price;
-  const neworder = {pizza:a,status:'ordered'}
+  const neworder = { pizza: a, status: "ordered", id: nextOrderId };
+  nextOrderId += 1;
   orderQueue.push(neworder);
   return neworder;
 }
+
+/**
+ * Challenge: write another utility function, completeOrder, that takes an orderId as a parameter
+ * finds the correct order in the orderQueue, and marks its status as "completed". For good measure,
+ * return the found order from the function.
+ *
+ * Note: you'll need to ensure that we're adding IDs to our orders when we create new orders. You can use a global `nextOrderId` variable and increment it every time a new order is created to simulate real IDs being managed for us by a database.
+ */
+function completeOrder(orderId) {
+  const order = orderQueue.find((ord) => ord.id === orderId);
+  order.status = "completed";
+  return order;
+}
+
+addNewPizza({ name: "Chicken Bacon Ranch", cost: 12 });
+addNewPizza({ name: "BBQ Chicken", cost: 12 });
+addNewPizza({ name: "Spicy Sausage", cost: 11 });
+
+placeOrder("Chicken Bacon Ranch");
+completeOrder("1");
+
+console.log("Menu:", menu);
+console.log("Cash in register:", cashInRegister);
+console.log("Order queue:", orderQueue);
