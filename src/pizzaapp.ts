c30145155f082727ed1e0b76c4f30b5b9e4fc176
AsokTamang <ashoktmg2058@gmail.com>
2025-12-18 14:04:04 -0500

    using the type while calling the function when the function is using generic types

--- src/pizzaapp.ts
+++ src/pizzaapp.ts
@@ -1,74 +1,83 @@
+//types
 type Pizza = {
-    id: number
-    name: string
-    price: number
-}
+  id: number;
+  name: string;
+  price: number;
+};
 
 type Order = {
-    id: number
-    pizza: Pizza
-    status: "ordered" | "completed"
-}
+  id: number;
+  pizza: Pizza;
+  status: "ordered" | "completed";
+};
 
-let cashInRegister = 100
-let nextOrderId = 1
-let nextPizzaId = 1
+//variables
+let cashInRegister = 100;
+let nextOrderId = 1;
+let nextPizzaId = 1;
 
 const menu: Pizza[] = [
-    { id: nextPizzaId++, name: "Margherita", price: 8 },
-    { id: nextPizzaId++, name: "Pepperoni", price: 10 },
-    { id: nextPizzaId++, name: "Hawaiian", price: 10 },
-    { id: nextPizzaId++, name: "Veggie", price: 9 },
-]
+  { id: nextPizzaId++, name: "Margherita", price: 8 },
+  { id: nextPizzaId++, name: "Pepperoni", price: 10 },
+  { id: nextPizzaId++, name: "Hawaiian", price: 10 },
+  { id: nextPizzaId++, name: "Veggie", price: 9 },
+];
+const orderQueue: Order[] = [];
 
-const orderQueue: Order[] = []
-
+//functions start here
 function addNewPizza(pizzaObj: Pizza): Pizza {
-    menu.push(pizzaObj)
-    return pizzaObj
+  menu.push(pizzaObj);
+  return pizzaObj;
 }
+addNewPizza({ id: nextPizzaId++, name: "Chicken Bacon Ranch", price: 12 });
+addNewPizza({ id: nextPizzaId++, name: "BBQ Chicken", price: 12 });
+addNewPizza({ id: nextPizzaId++, name: "Spicy Sausage", price: 11 });
 
 function placeOrder(pizza: Pizza): Order | undefined {
-    const newOrder: Order = { id: nextOrderId++, pizza: pizza, status: "ordered" }
-    orderQueue.push(newOrder)
-    cashInRegister += pizza.price
-    return newOrder
+  const newOrder: Order = {
+    id: nextOrderId++,
+    pizza: pizza,
+    status: "ordered",
+  };
+  orderQueue.push(newOrder);
+  cashInRegister += pizza.price;
+  return newOrder;
 }
 
-
-function addToArray<Type>(array:Type[], item:Type) { //here we are using Generic function of Ts inorder to use any kind of types in our function
-  //which makes it flexible to pass any type of arguments while calling this addToArray function
-    array.push(item)
-    return array
+function addToArray<Type>(array: Type[], item: Type) {
+  //here we are using Generic function of Ts inorder to use any kind of types in our function
+  //which makes it flexible to pass any type of arguments while calling this addToArray fu
+  array.push(item);
+  return array;
 }
-
-// example usage:
-addToArray(menu, {id: nextPizzaId++, name: "Chicken Bacon Ranch", price: 12 })
-addToArray(orderQueue, { id: nextOrderId++, pizza: menu[2], status: "completed" })
-
-
-
+addToArray(menu, { id: nextPizzaId++, name: "Chicken Bacon Ranch", price: 12 });
+addToArray<Order>(orderQueue, {
+  id: nextOrderId++,
+  pizza: menu[2]!,
+  status: "ordered",
+}); //here as we are using generic function in addToArray function, we also must define the type of argument that is passed in this function if the passed argument's type
+//must be some specific types or values like here status must be 'ordered' | 'completed'
 
 function completeOrder(orderId: number): Order | undefined {
-    const order = orderQueue.find(order => order.id === orderId)
-    if (!order) {
-        console.error(`${orderId} was not found in the orderQueue`)
-        return
-    }
-    order.status = "completed"
-    return order
+  const order = orderQueue.find((order) => order.id === orderId);
+  if (!order) {
+    console.error(`${orderId} was not found in the orderQueue`);
+    return;
+  }
+  order.status = "completed";
+  return order;
 }
 
 export function getPizzaDetail(identifier: string | number): Pizza | undefined {
-    if (typeof identifier === "string") {
-        return menu.find(pizza => pizza.name.toLowerCase() === identifier.toLowerCase())
-    } else if (typeof identifier === "number") {
-        return menu.find(pizza => pizza.id === identifier)
-    } else {
-        throw new TypeError("Parameter `identifier` must be either a string or a number")
-    }
+  if (typeof identifier === "string") {
+    return menu.find(
+      (pizza) => pizza.name.toLowerCase() === identifier.toLowerCase()
+    );
+  } else if (typeof identifier === "number") {
+    return menu.find((pizza) => pizza.id === identifier);
+  } else {
+    throw new TypeError(
+      "Parameter `identifier` must be either a string or a number"
+    );
+  }
 }
-
-addNewPizza({ id: nextPizzaId++, name: "Chicken Bacon Ranch", price: 12 })
-addNewPizza({ id: nextPizzaId++, name: "BBQ Chicken", price: 12 })
-addNewPizza({ id: nextPizzaId++, name: "Spicy Sausage", price: 11 })
